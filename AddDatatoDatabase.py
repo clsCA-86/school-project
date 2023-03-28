8a7550e234cfe73ac2f2695837d22f0f5da3cf2b
clsCA-86 <123132398+clsCA-86@users.noreply.github.com>
2023-03-28 08:04:45 -0700

    Update AddDatatoDatabase.py

--- AddDatatoDatabase.py
+++ AddDatatoDatabase.py
@@ -2,10 +2,7 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
-cred = credentials.Certificate("serviceAccountKey.json")
-firebase_admin.initialize_app(cred, {
-    'databaseURL': "https://faceattendacerealtime-4f391-default-rtdb.firebaseio.com/"
-})
+
 
 ref = db.reference('Students')
 
